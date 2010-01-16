57d8469d8e819b748ec9a254f0f9ed5cfadc1713
Daniel Berger <djberg96@gmail.com>
2010-01-16 08:05:44 -0700

    Reorganized the file layout, updated the gemspec, README and test file.

--- archive-tar-external.gemspec
+++ archive-tar-external.gemspec
@@ -1,40 +1,40 @@
 require 'rubygems'
 require 'rbconfig'
 
- Gem::Specification.new do |s|
-   s.name      = 'archive-tar-external'
-   s.version   = '1.2.3'
-   s.summary   = 'A simple way to create tar archives using external calls'
-   s.license   = 'Artistic 2.0'
-   s.author    = 'Daniel Berger'
-   s.email     = 'djberg96@gmail.com'
-   s.homepage  = 'http://www.rubyforge.org/shards'
-   s.test_file = 'test/test_archive_tar_external.rb'
-   s.has_rdoc  = true
-   s.files     = Dir['**/*'].reject{ |f| f.include?('CVS') }
+Gem::Specification.new do |spec|
+  spec.name      = 'archive-tar-external'
+  spec.version   = '1.3.0'
+  spec.summary   = 'A simple way to create tar archives using external calls'
+  spec.license   = 'Artistic 2.0'
+  spec.author    = 'Daniel Berger'
+  spec.email     = 'djberg96@gmail.com'
+  spec.homepage  = 'http://www.rubyforge.org/shards'
+  spec.test_file = 'test/test_archive_tar_external.rb'
+  spec.has_rdoc  = true
+  spec.files     = Dir['**/*'].reject{ |f| f.include?('git') }
 
-   s.rubyforge_project = 'shards'
+  spec.rubyforge_project = 'shards'
 
-   s.extra_rdoc_files = [
-      'README',
-      'CHANGES',
-      'MANIFEST',
-      'doc/tar_external.txt'
-   ]
+  spec.extra_rdoc_files = [
+    'README',
+    'CHANGES',
+    'MANIFEST',
+    'doc/tar_external.txt'
+  ]
 
-   s.description = <<-EOF
-      The archive-tar-external is a simple wrapper interface for creating
-      tar files using your system's tar command. You can also easily compress
-      your tar files with your system's compression programs such as zip, gzip,
-      or bzip2.
-   EOF
+  spec.description = <<-EOF
+    The archive-tar-external is a simple wrapper interface for creating
+    tar files using your system's tar command. You can also easily compress
+    your tar files with your system's compression programs such as zip, gzip,
+    or bzip2.
+  EOF
 
-   s.add_development_dependency('test-unit', '>= 2.0.3')
-   s.add_development_dependency('ptools', '>= 1.1.7')
+  spec.add_development_dependency('test-unit', '>= 2.0.3')
+  spec.add_development_dependency('ptools', '>= 1.1.7')
 
-   if Config::CONFIG['host_os'] =~ /mswin|dos|win32|cygwin|mingw|win32/i
-      if RUBY_VERSION.to_f < 1.9 && RUBY_PLATFORM !~ /java/i
-         s.add_dependency('win32-open3', '>= 0.3.1')
-      end
-   end
+  if Config::CONFIG['host_os'] =~ /mswin|dos|win32|cygwin|mingw|win32/i
+    if RUBY_VERSION.to_f < 1.9 && RUBY_PLATFORM !~ /java/i
+      spec.add_dependency('win32-open3', '>= 0.3.1')
+    end
+  end
 end
--- test/test_archive_tar_external.rb
+++ test/test_archive_tar_external.rb
@@ -7,195 +7,197 @@
 require 'rubygems'
 gem 'test-unit'
 
-require 'archive/tar_external'
+require 'archive/tar/external'
 require 'test/unit'
 require 'ptools'
 include Archive
 
 class TC_ArchiveTarExternal < Test::Unit::TestCase
-   def self.startup
-      Dir.chdir(File.dirname(File.expand_path(__FILE__)))
+  def self.startup
+    Dir.chdir(File.dirname(File.expand_path(__FILE__)))
 
-      @@tmp_file1 = 'temp1.txt'
-      @@tmp_file2 = 'temp2.txt'
-      @@tmp_file3 = 'temp3.txt'
+    @@tmp_file1 = 'temp1.txt'
+    @@tmp_file2 = 'temp2.txt'
+    @@tmp_file3 = 'temp3.txt'
 
-      @@gtar_found  = File.which('gtar')
-      @@tar_found   = File.which('tar')
-      @@gzip_found  = File.which('gzip')
-      @@bzip2_found = File.which('bzip2')
+    @@gtar_found  = File.which('gtar')
+    @@tar_found   = File.which('tar')
+    @@gzip_found  = File.which('gzip')
+    @@bzip2_found = File.which('bzip2')
 
-      File.open(@@tmp_file1, 'w'){ |f| f.puts 'This is a temporary text file' }
-      File.open(@@tmp_file2, 'w'){ |f| f.puts 'This is a temporary text file' }
-      File.open(@@tmp_file3, 'w'){ |f| f.puts 'This is a temporary text file' }
-   end
+    File.open(@@tmp_file1, 'w'){ |f| f.puts 'This is a temporary text file' }
+    File.open(@@tmp_file2, 'w'){ |f| f.puts 'This is a temporary text file' }
+    File.open(@@tmp_file3, 'w'){ |f| f.puts 'This is a temporary text file' }
+  end
 
-   def setup
-      @tar      = Tar::External.new('test.tar')
-      @tar_name = 'test.tar'
-      @pattern  = '*.txt'
-      @archive  = 'temp.tar.gz'
-   end
+  def setup
+    @tar      = Tar::External.new('test.tar')
+    @tar_name = 'test.tar'
+    @pattern  = '*.txt'
+    @archive  = 'temp.tar.gz'
+  end
 
-   def test_version
-      assert_equal('1.2.3', Tar::External::VERSION)
-   end
+  def test_version
+    assert_equal('1.3.0', Tar::External::VERSION)
+  end
 
-   def test_constructor
-      assert_nothing_raised{ Tar::External.new(@tar_name) }
-   end
+  def test_constructor
+    assert_nothing_raised{ Tar::External.new(@tar_name) }
+  end
 
-   def test_constructor_with_extension
-      assert_nothing_raised{ Tar::External.new(@tar_name, '*.txt') }
-   end
+  def test_constructor_with_extension
+    assert_nothing_raised{ Tar::External.new(@tar_name, '*.txt') }
+  end
 
-   def test_constructor_with_program
-      omit_unless(@@gzip_found){ 'gzip program not found - skipping' }
-      assert_nothing_raised{ Tar::External.new(@tar_name, '*.txt', 'gzip') }
-   end
+  def test_constructor_with_program
+    omit_unless(@@gzip_found){ 'gzip program not found - skipping' }
+    assert_nothing_raised{ Tar::External.new(@tar_name, '*.txt', 'gzip') }
+  end
 
-   def test_constructor_expected_errors
-      assert_raise(ArgumentError){ Tar::External.new }
-   end
+  def test_constructor_expected_errors
+    assert_raise(ArgumentError){ Tar::External.new }
+  end
 
-   def test_tar_program
-      assert_respond_to(@tar, :tar_program)
-      assert_equal('tar', @tar.tar_program)
-   end
+  def test_tar_program
+    assert_respond_to(@tar, :tar_program)
+    assert_equal('tar', @tar.tar_program)
+  end
 
-   def test_archive_name
-      assert_respond_to(@tar, :archive_name)
-      assert_respond_to(@tar, :archive_name=)
+  def test_archive_name
+    assert_respond_to(@tar, :archive_name)
+    assert_respond_to(@tar, :archive_name=)
 
-      assert_equal('test.tar', @tar.archive_name)
-      assert_nothing_raised{ @tar.archive_name }
-      assert_nothing_raised{ @tar.archive_name = 'foo' }
-   end
+    assert_equal('test.tar', @tar.archive_name)
+    assert_nothing_raised{ @tar.archive_name }
+    assert_nothing_raised{ @tar.archive_name = 'foo' }
+  end
 
-   def test_compressed_archive_name_get
-      assert_respond_to(@tar, :compressed_archive_name)
-      assert_nil(@tar.compressed_archive_name)
-   end
+  def test_compressed_archive_name_get
+    assert_respond_to(@tar, :compressed_archive_name)
+    assert_nil(@tar.compressed_archive_name)
+  end
 
-   def test_compressed_archive_name_set
-      assert_respond_to(@tar, :compressed_archive_name=)
-      assert_nothing_raised{ @tar.compressed_archive_name = 'test.tar.gz' }
-      assert_equal('test.tar.gz', @tar.compressed_archive_name)
-      assert_equal('test.tar', @tar.archive_name)
+  def test_compressed_archive_name_set
+    assert_respond_to(@tar, :compressed_archive_name=)
+    assert_nothing_raised{ @tar.compressed_archive_name = 'test.tar.gz' }
+    assert_equal('test.tar.gz', @tar.compressed_archive_name)
+    assert_equal('test.tar', @tar.archive_name)
 
-      assert_nothing_raised{ @tar.compressed_archive_name = 'test.tgz' }
-      assert_equal('test.tgz', @tar.compressed_archive_name)
-      assert_equal('test.tar', @tar.archive_name)
-   end
+    assert_nothing_raised{ @tar.compressed_archive_name = 'test.tgz' }
+    assert_equal('test.tgz', @tar.compressed_archive_name)
+    assert_equal('test.tar', @tar.archive_name)
+  end
 
-   def test_create_archive_basic
-      assert_respond_to(@tar, :create_archive)
+  def test_create_archive_basic
+    assert_respond_to(@tar, :create_archive)
 
-      assert_raises(ArgumentError){ @tar.create_archive }
-      assert_raises(Tar::Error){ @tar.create_archive('*.blah') }
+    assert_raises(ArgumentError){ @tar.create_archive }
+    assert_raises(Tar::Error){ @tar.create_archive('*.blah') }
 
-      assert_nothing_raised{ @tar.create_archive(@pattern) }
-      assert_true(File.exists?(@tar_name))
-   end
+    assert_nothing_raised{ @tar.create_archive(@pattern) }
+    assert_true(File.exists?(@tar_name))
+  end
 
-   def test_create_alias
-      assert_respond_to(@tar, :create)
-      assert_true(Tar::External.instance_method(:create) == Tar::External.instance_method(:create_archive))
-   end
+  def test_create_alias
+    assert_respond_to(@tar, :create)
+    assert_true(Tar::External.instance_method(:create) == Tar::External.instance_method(:create_archive))
+  end
 
-   def test_compress_archive_basic
-      assert_respond_to(@tar, :compress_archive)
-   end
+  def test_compress_archive_basic
+    assert_respond_to(@tar, :compress_archive)
+  end
 
-   def test_compress_alias
-      assert_respond_to(@tar, :compress)
-      assert_true(Tar::External.instance_method(:compress) == Tar::External.instance_method(:compress_archive))
-   end
+  def test_compress_alias
+    assert_respond_to(@tar, :compress)
+    assert_true(Tar::External.instance_method(:compress) == Tar::External.instance_method(:compress_archive))
+  end
 
-   def test_compress_archive_gzip
-      assert_nothing_raised{ @tar.create_archive('*.txt') }
-      assert_nothing_raised{ @tar.compress_archive }
+  def test_compress_archive_gzip
+    assert_nothing_raised{ @tar.create_archive('*.txt') }
+    assert_nothing_raised{ @tar.compress_archive }
 
-      assert_equal('test.tar.gz', @tar.compressed_archive_name)
-      assert_true(File.exists?('test.tar.gz'))
-   end
+    assert_equal('test.tar.gz', @tar.compressed_archive_name)
+    assert_true(File.exists?('test.tar.gz'))
+  end
 
-   def test_compress_archive_bzip2
-      assert_nothing_raised{ @tar.create_archive('*.txt') }
-      assert_nothing_raised{ @tar.compress_archive('bzip2') }
-      assert_true(File.exists?('test.tar.bz2'))
-   end
+  def test_compress_archive_bzip2
+    assert_nothing_raised{ @tar.create_archive('*.txt') }
+    assert_nothing_raised{ @tar.compress_archive('bzip2') }
+    assert_true(File.exists?('test.tar.bz2'))
+  end
 
-   def test_uncompress_archive
-      assert_respond_to(@tar, :uncompress_archive)
-      assert_nothing_raised{ @tar.create_archive('*.txt') }
-      assert_nothing_raised{ @tar.compress_archive }
-      assert_nothing_raised{ @tar.uncompress_archive }
-      assert_false(File.exists?('test.tar.gz'))
-   end
+  def test_uncompress_archive
+    assert_respond_to(@tar, :uncompress_archive)
+    assert_nothing_raised{ @tar.create_archive('*.txt') }
+    assert_nothing_raised{ @tar.compress_archive }
+    assert_nothing_raised{ @tar.uncompress_archive }
+    assert_false(File.exists?('test.tar.gz'))
+  end
 
-   def test_uncompress_archive_class_method
-      assert_respond_to(Tar::External, :uncompress_archive)
-   end
+  def test_uncompress_archive_class_method
+    assert_respond_to(Tar::External, :uncompress_archive)
+  end
 
-   def test_uncompress_alias
-      assert_respond_to(Tar::External, :uncompress)
-      assert_true(Tar::External.method(:uncompress) == Tar::External.method(:uncompress_archive))
-   end
+  def test_uncompress_alias
+    assert_respond_to(Tar::External, :uncompress)
+    assert_true(Tar::External.method(:uncompress) == Tar::External.method(:uncompress_archive))
+  end
 
-   def test_archive_info
-      assert_respond_to(@tar, :archive_info)
-      assert_nothing_raised{ @tar.create_archive('*.txt') }
-      assert_equal(['temp1.txt','temp2.txt','temp3.txt'], @tar.archive_info)
-   end
+  def test_archive_info
+    assert_respond_to(@tar, :archive_info)
+    assert_nothing_raised{ @tar.create_archive('*.txt') }
+    assert_equal(['temp1.txt','temp2.txt','temp3.txt'], @tar.archive_info)
+  end
 
-   def test_add_to_archive
-      assert_respond_to(@tar,:add_to_archive)
-      assert_nothing_raised{ @tar.create_archive('temp1.txt') }
-      assert_nothing_raised{ @tar.add_to_archive('temp2.txt') }
-      assert_nothing_raised{ @tar.add_to_archive('temp2.txt','temp3.txt') }
-   end
+  def test_add_to_archive
+    assert_respond_to(@tar,:add_to_archive)
+    assert_nothing_raised{ @tar.create_archive('temp1.txt') }
+    assert_nothing_raised{ @tar.add_to_archive('temp2.txt') }
+    assert_nothing_raised{ @tar.add_to_archive('temp2.txt','temp3.txt') }
+  end
 
-   def test_update_archive
-      assert_respond_to(@tar, :update_archive)
-      assert_nothing_raised{ @tar.create_archive('*.txt') }
-      assert_nothing_raised{ @tar.update_archive('temp2.txt') }
-   end
+  def test_update_archive
+    assert_respond_to(@tar, :update_archive)
+    assert_nothing_raised{ @tar.create_archive('*.txt') }
+    assert_nothing_raised{ @tar.update_archive('temp2.txt') }
+  end
 
-   def test_extract_archive_basic
-      assert_respond_to(@tar, :extract_archive)
-   end
+  def test_extract_archive_basic
+    assert_respond_to(@tar, :extract_archive)
+  end
 
-   def test_extract_archive_aliases
-      assert_true(Tar::External.instance_method(:extract_archive) == Tar::External.instance_method(:expand_archive)) 
-      assert_true(Tar::External.instance_method(:extract) == Tar::External.instance_method(:expand_archive)) 
-      assert_true(Tar::External.instance_method(:expand) == Tar::External.instance_method(:expand_archive)) 
-   end
+  def test_extract_archive_aliases
+    assert_true(Tar::External.instance_method(:extract_archive) == Tar::External.instance_method(:expand_archive)) 
+    assert_true(Tar::External.instance_method(:extract) == Tar::External.instance_method(:expand_archive)) 
+    assert_true(Tar::External.instance_method(:expand) == Tar::External.instance_method(:expand_archive)) 
+  end
 
-   def test_extract_archive_advanced
-      assert_nothing_raised{ @tar.tar_program = @@gtar } if Config::CONFIG['host_os'] =~ /sunos|solaris/i
-      assert_nothing_raised{ @tar.create('*.txt') }
-      assert_raises(Tar::Error){ @tar.expand('blah.txt') }
+  def test_extract_archive_advanced
+    omit_unless(Config::CONFIG['host_os'] =~ /sunos|solaris/){
+      assert_nothing_raised{ @tar.tar_program = @@gtar } 
+    }
+    assert_nothing_raised{ @tar.create('*.txt') }
+    assert_raises(Tar::Error){ @tar.expand('blah.txt') }
 
-      assert_nothing_raised{ @tar.extract_archive }
-      assert_nothing_raised{ @tar.extract_archive('temp2.txt') }
-   end
+    assert_nothing_raised{ @tar.extract_archive }
+    assert_nothing_raised{ @tar.extract_archive('temp2.txt') }
+  end
 
-   def teardown
-      @tar = nil
-      File.delete('test.tar') if File.exists?('test.tar')
-      File.delete('test.tar.gz') if File.exists?('test.tar.gz')
-      File.delete('test.tar.bz2') if File.exists?('test.tar.bz2')
-      File.delete('test.tar.zip') if File.exists?('test.tar.zip')
-   end
+  def teardown
+    @tar = nil
+    File.delete('test.tar') if File.exists?('test.tar')
+    File.delete('test.tar.gz') if File.exists?('test.tar.gz')
+    File.delete('test.tar.bz2') if File.exists?('test.tar.bz2')
+    File.delete('test.tar.zip') if File.exists?('test.tar.zip')
+  end
 
-   def self.shutdown
-      @@tar_foudn   = nil
-      @@gzip_found  = nil
-      @@bzip2_found = nil
+  def self.shutdown
+    @@tar_foudn   = nil
+    @@gzip_found  = nil
+    @@bzip2_found = nil
 
-      File.delete(@@tmp_file1) if File.exists?(@@tmp_file1)
-      File.delete(@@tmp_file2) if File.exists?(@@tmp_file2)
-      File.delete(@@tmp_file3) if File.exists?(@@tmp_file3)
-   end
+    File.delete(@@tmp_file1) if File.exists?(@@tmp_file1)
+    File.delete(@@tmp_file2) if File.exists?(@@tmp_file2)
+    File.delete(@@tmp_file3) if File.exists?(@@tmp_file3)
+  end
 end
