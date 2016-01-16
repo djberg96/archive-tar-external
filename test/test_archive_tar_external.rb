5f607024951fab3771c9c5e3ca4b762e5a754640
Daniel Berger <dberger@redhat.com>
2016-01-16 06:55:51 -0700

    Version bump.

--- test/test_archive_tar_external.rb
+++ test/test_archive_tar_external.rb
@@ -35,7 +35,7 @@ class TC_ArchiveTarExternal < Test::Unit::TestCase
   end
 
   def test_version
-    assert_equal('1.3.2', Tar::External::VERSION)
+    assert_equal('1.3.3', Tar::External::VERSION)
   end
 
   def test_constructor
